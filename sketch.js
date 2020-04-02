28d1b50873a479d72f835f95ddc85e023448e4ce
krishna1220 <59966007+krishna1220@users.noreply.github.com>
2020-04-02 19:54:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var ball;
+var database,position;
+var gameState=0,playerCount=0,form,player,game;
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+}
+
+function draw(){
+    
+    background("white");
+}
